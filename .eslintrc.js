45a895c9404cc2f32e2245ba7de1cba02a1b6003
Lave <cnyingfu@163.com>
2018-08-02 14:55:18 +0800

    init  project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint-config-airbnb",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+};
---
+++ test.js
@@ -0,0 +1,4 @@
+const a = 0
+const b = a + 1
+
+console.log(b)
